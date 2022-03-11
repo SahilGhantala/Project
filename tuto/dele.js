eee9ad8eae1ae5b0f4de044b32c587a093ef01dc
jaldipbhalani <jaldipbhalani@gmail.com>
2022-03-11 22:40:47 +0530

    11-03-2022

--- tuto/dele.js
+++ tuto/dele.js
@@ -1,8 +1,13 @@
-var emp=[1,2,6,9]
-console.log(emp)
+// var emp=[1,2,6,9]
+// console.log(emp)
 
 
-var emp=new Array("Jai","Vijay","Smith");
-// var emp= ["Jai","Vijay","Smith"];
-console.log(emp)
-  
+// var emp=new Array("Jai","Vijay","Smith");
+// // var emp= ["Jai","Vijay","Smith"];
+// console.log(emp)
+
+// var str="hello world";
+// console.log(str)
+
+var str= new String("javaScript tuto")
+console.log(str)
